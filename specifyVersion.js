03c2663b9b95b39cea297bd9a566ca17267ed227
Asaf Romano <aromano@preempt.com>
2017-10-03 11:34:56 +0300

    Update to mongo 3.4.0

--- specifyVersion.js
+++ specifyVersion.js
@@ -3,17 +3,17 @@ let {Mockgoose} = require('./built/mockgoose');
 const {MongoDBDownload} = require('mongodb-download');
 
 let mockgoose = new Mockgoose(mongoose);
-mockgoose.helper.setDbVersion("3.2.1");
+mockgoose.helper.setDbVersion("3.4.0");
 
 mockgoose.prepareStorage().then(() => {
 	console.log('prepare storage ok', mongoose.mocked);
 	mongoose.connect('mongodb://sdfsdfsdf:27017');
-	
-	mongoose.connection.on('connected', function () {  
+
+	mongoose.connection.on('connected', function () {
 	  console.log('Mongoose open');
-	}); 
+	});
 });
 //var Mockgoose = require('./Mockgoose')(mongoose).then(function() {
-	
+
 
 //});
